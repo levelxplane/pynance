42dc303f20ac61b2be1211a85582f840f12b6e77
Mansoor <mansoor@Mansoors-MacBook-Pro.local>
2018-02-12 14:27:11 -0800

    added graphs and stuff

--- lib/market.py
+++ lib/market.py
@@ -3,7 +3,7 @@ import csv, time, datetime, os, requests, sys, json, logging
 BASE_PATH=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_PATH)
 
-from lib import config
+from lib import config, general
 
 # # enable REQUESTS logging
 # logging.basicConfig()
@@ -40,12 +40,46 @@ def agg_trades(symbol):
     response = requests.get(endpoint, params=query)
     return ( response.json() )
 
-def candlestick(symbol, interval):
+def candlestick(symbol, interval, limit=1):
     endpoint = general.generate_endpoint('/api/v1/klines')
-    query={'symbol':symbol, 'interval':interval }
+    query={'symbol':symbol, 'interval':interval, 'limit':limit}
     response = requests.get(endpoint, params=query)
-    return ( response.json() )
 
+    tmp_list=[]
+    for x in response.json():
+        tmp_list.append({"openTime":x[0],
+            "open":x[1],
+            "high":x[2],
+            "low":x[3],
+            "close":x[4],
+            "volume":x[5],
+            "close_time":x[6],
+            "quote_asset_volume":x[7],
+            "number_of_trades":x[8],
+            "taker_buy_base_asset_volume":x[9],
+            "taker_buy_quote_asset_volume":x[10],
+            "ignore":x[11]
+        })
+    return tmp_list
+
+
+    # return ( response.json() )
+# [
+#   [
+#     1499040000000,      // "Open time":x[],
+#     "0.01634790",       // "Open":x[],
+#     "0.80000000",       // "High":x[],
+#     "0.01575800",       // "Low":x[],
+#     "0.01577100",       // "Close":x[],
+#     "148976.11427815",  // "Volume":x[],
+#     1499644799999,      // "Close time":x[],
+#     "2434.19055334",    // "Quote asset volume":x[],
+#     308,                // "Number of trades":x[],
+#     "1756.87402397",    // "Taker buy base asset volume":x[],
+#     "28.46694368",      // "Taker buy quote asset volume":x[],
+#     "17928899.62484339" // "Ignore":x[],
+#   ]
+# ]
 
 def ticker(symbol=None):
     endpoint = general.generate_endpoint('/api/v1/ticker/24hr')
